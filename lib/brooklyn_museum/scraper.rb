5777ed4d362be821a3f1f5d4319f1a73047a1327
Timur Mishiev <timurmishiev@gmail.com>
2020-03-12 21:40:05 +0000

    write methods in scraper class

--- lib/brooklyn_museum/scraper.rb
+++ lib/brooklyn_museum/scraper.rb
@@ -1,3 +1,6 @@
 class BrooklynMuseum::SCRAPER
-  
+      def scrape_exhibitions
+        page = Nokogiri::HTML(open("https://www.brooklynmuseum.org/exhibitions"))
+        binding.pry
+      end 
 end 
