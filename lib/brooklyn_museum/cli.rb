c7306c8394fe4a64008e430f4ee8407ba0c1b9d0
Timur Mishiev <timurmishiev@gmail.com>
2020-03-14 01:14:39 +0000

    colplete remake

--- lib/brooklyn_museum/cli.rb
+++ lib/brooklyn_museum/cli.rb
@@ -1,44 +1,33 @@
 class BrooklynMuseum::CLI
   def call 
     puts "\nWelcome to Brooklyn Museum!\n"
-     get_list_of_exhibits
-     list_these_exhibit
-     get_user_input
+    
+     
   end 
   
   def get_list_of_exhibits
      
-    @names = BrooklynMuseum::EXHIBITIONS.all
+  
    
   end 
   
   def list_these_exhibit
-    puts "Choose an exhibit to see a description"
-    @names.each.with_index(1) do |exhibit, index|
-      puts "#{index}. #{exhibit.title}"
-  end 
+  
+   
  end
   
   def get_user_input
-    chosen_exhibit = gets.strip.to_i
-    show_description_for(chosen_exhibit) if valid_input(chosen_exhibit, @names)
+   
    end
    
    def valid_input(input, data)
-    input <= data.length && input > 0
+    
   end 
   
   def show_description_for(chosen_exhibit)
-    exhibit = @names[chosen_exhibit-1]
+
     
- 
-    #dispays descriptions
-     BrooklynMuseum::EXHIBITIONS.get_descriptions
-     puts "Here is a few words about #{exhibit.title}"
-    #displays dates 
-    BrooklynMuseum::EXHIBITIONS.get_dates 
-    puts "Here are the dates of  #{exhibit.title}"
   
-  end 
+ 
 end 
   
--- lib/brooklyn_museum/exhibitions.rb
+++ lib/brooklyn_museum/exhibitions.rb
@@ -1,35 +1,5 @@
 class BrooklynMuseum::EXHIBITIONS
   
   
-  attr_accessor  :descriptions, :dates
-   @@all = []
-  
- 
-   
-   def initialize(descriptions)
-     
-     @descriptions = descriptions
-     @dates = dates 
-     #title.descriptions << self
-     save 
-   end 
-   
-   def self.all
-     #BrooklynMuseum::SCRAPER.scrape_exhibitions if @@all.empty?
-     @@all
-   end 
-   
-   def save 
-     @@all << self 
-   end 
-   
-   def get_descriptions
-    BrooklynMuseum::SCRAPER.scrape_descriptions
-   end
-   
-   def get_dates 
-    BrooklynMuseum::SCRAPER.scrape_dates
-   end 
-   
   
 end 
